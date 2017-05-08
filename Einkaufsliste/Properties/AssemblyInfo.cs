f9891cc2547b32ad5c37b598b774779273cf9e2e
romankl1m <romnklymko@gmail.com>
2017-05-08 20:36:28 +0200

    Mehrsprachlichkeit

--- Einkaufsliste/Properties/AssemblyInfo.cs
+++ Einkaufsliste/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,11 +7,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Einkaufslisten_App")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Einkaufslisten anlegen, bearbeiten, löschen")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Christian Lange, Roman Klymko")]
 [assembly: AssemblyProduct("Einkaufslisten_App")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -27,3 +28,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
+//[assembly: NeutralResourcesLanguage("de")]
